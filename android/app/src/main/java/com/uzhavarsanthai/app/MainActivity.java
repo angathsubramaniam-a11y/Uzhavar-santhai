1d13dd2acb14e35f0c5ba0fc1aa7ce9f340a92a9
Angesh2007 <angeshangath020@gmail.com>
2026-05-24 11:15:44 +0530

    Updated project

---
+++ android/app/src/main/java/com/uzhavarsanthai/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.uzhavarsanthai.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Uzhavar Santhai',
+        short_name: 'UzhavarSanthai',
+        description: 'Uzhavar Santhai App',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
